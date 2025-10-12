644703df93441527d031f2d10f184df76e5edf04
oxxultus <a38027928@gmail.com>
2025-10-12 23:56:15 +0900

    메인 서버 프로젝트 초기 파일 업로드

---
+++ src/test/java/com/aeranghae/aeranghaemainapi/AeranghaeMainApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aeranghae.aeranghaemainapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AeranghaeMainApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
