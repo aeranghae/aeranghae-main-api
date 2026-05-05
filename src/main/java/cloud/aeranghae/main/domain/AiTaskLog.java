1070782dd64b00e88a2438bb341fd2dfeea37623
chan jeong <jos103901@naver.com>
2026-05-05 22:01:09 +0900

    test

--- src/main/java/cloud/aeranghae/main/domain/AiTaskLog.java
+++ src/main/java/cloud/aeranghae/main/domain/AiTaskLog.java
@@ -11,7 +11,7 @@ import lombok.*;
 @Table(name = "ai_tasks")
 public class AiTaskLog {
     @Id
-    private Long id; // AiTask의 ID와 매핑
+    private Long id; // AiTask의 ID와 매핑함
 
     @OneToOne(fetch = FetchType.LAZY)
     @MapsId // 🌟 AiTask의 ID를 PK로 사용하도록 연결
